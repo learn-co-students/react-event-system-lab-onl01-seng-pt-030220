9baf8bbe9aa0e24be1437fdd1806f6e03c198334
Bouloute <annaick1995@gmail.com>
2020-11-04 21:16:11 -0500

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
-// Code EyesOnMe Component Here
+import React, { Component } from 'react';
+
+class EyesOnMe extends Component {
+    look = () => {
+        console.log("Good!")
+    }
+
+    dontLook = () => {
+        console.log("Hey! Eyes on me!")
+    }
+
+    render() {
+        return (
+            <div>
+                <button onFocus={this.look} onBlur={this.dontLook}></button>
+            </div>
+        );
+    }
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,17 @@
-// Code Keypad Component Here
+import React, { Component } from 'react';
+
+class Keypad extends Component {
+    keyUpHandler = (event) => {
+        console.log('Entering password...')
+    }
+
+    render() {
+        return (
+            <div>
+                <input onKeyUp={this.keyUpHandler} type="password"></input> 
+            </div>
+        );
+    }
+}
+
+export default Keypad;
