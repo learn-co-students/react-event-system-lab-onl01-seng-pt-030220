c95465c9a8431f779acc6f506b2f09d777b0719c
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-11-21 20:41:32 +0000

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,8 @@
 // Code EyesOnMe Component Here
+
+const onBlur = () => {return console.log('Hey! Eyes on me!')}
+const onFocus = () => {return console.log('Good')}
+export default const EyesOnMe = () => {
+  
+  return <button onBlur = {handleOnBlur} onFocus = {handleOnFocus}/>
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,12 @@
 // Code Keypad Component Here
+import React from 'react'
+
+class EyesOnMe extends React.Component {
+
+  const handleKeyUp = () => {
+    return console.log('Entering password . . .')
+  }
+  render = () => {
+    return <input type="password" onKeyUp={this.handleKeyUp}/>
+  }
+}
